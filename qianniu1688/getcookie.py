8f52cc85bbd2bb09a4387c8f7183ee43bac8b80e
Lky965823- <583215672@qq.com>
2022-11-08 23:51:38 +0800

    捕获cookie工具

---
+++ qianniu1688/getcookie.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from time import sleep
+
+option = ChromeOptions()
+option.add_experimental_option('excludeSwitches', ['enable-automation'])
+option.add_argument("--disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(options=option)
+url = 'https://item.taobao.com/item.htm?spm=a1z10.3-c.w4002-24537816225.9.6bfd6f73yJ9Wts&id=682628841629'
+driver.get(url)
+username = driver.find_element_by_id("fm-login-id")
+username.click()
+username.send_keys("15521080769")
+password = driver.find_element_by_id("fm-login-password")
+password.click()
+password.send_keys("Lky965823-")
+submit = driver.find_element_by_xpath("//*[@id='login-form']/div[4]/button")
+submit.click()
+sleep(3)
+
+cookieset = []
+i = 1
+while i < 15:
+    driver.get(url)
+    cookie_list = driver.get_cookies()
+    cookie = [item["name"] + "=" + item["value"] for item in cookie_list]
+    cookiestr = ';'.join(item for item in cookie)
+    # print(cookie_list)
+    # print(cookiestr)
+    cookie_dict = {}
+    cookie_dict['Cookies'] = cookiestr
+    cookieset.append(cookie_dict)
+    # print("\n")
+    # print(cookieset)
+    print("第" + str(i)+"个cookie抓取成功")
+    i += 1
+
+    sleep(3)
+print(cookieset)
+driver.quit()
