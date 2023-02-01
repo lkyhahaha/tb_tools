46fec9eea5e07b761576745a94fd0ab67a465c96
lkyhahaha <80745908+lkyhahaha@users.noreply.github.com>
2023-02-01 17:09:14 +0800

    Delete test_baidu.py

--- test_baidu.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("https://www.baidu.com")
-
-driver.find_element_by_id("kw").send_keys("Selenium")
-driver.find_element_by_id("su").click()
-driver.quit()
